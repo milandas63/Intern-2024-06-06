b702a3cadb0ab65e22b717772811e7f151a52268
Deb Prakash Swain <150893454+PRAKASH-14A@users.noreply.github.com>
2024-06-19 23:28:33 +0530

    Update Assignment-1.java

--- Assignment/Deba Prakash Swain/Day-9/Assignment-1.java
+++ Assignment/Deba Prakash Swain/Day-9/Assignment-1.java
@@ -1,1 +1,21 @@
+public class Question4_Partten {
+    public static void main(String[] args) {
+        int height = 10;
+        int lastLine = 2 * height - 1;
 
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < lastLine; j++) {
+                if (j == height - 1 - i || j == height - 1 + i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < lastLine; i++) {
+            System.out.print("*");
+        }
+    }
+}
