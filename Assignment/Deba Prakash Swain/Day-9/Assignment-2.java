8926c433599d2b598096355afa33b01d603ad17e
Deb Prakash Swain <150893454+PRAKASH-14A@users.noreply.github.com>
2024-06-20 00:02:02 +0530

    Update Assignment-2.java

--- Assignment/Deba Prakash Swain/Day-9/Assignment-2.java
+++ Assignment/Deba Prakash Swain/Day-9/Assignment-2.java
@@ -1,1 +1,18 @@
+public class Question_5Partten{
+    public static void main(String[] args){
+        int height = 10;
 
+        for (int i=0;i<height;i++){
+            for (int j=0;j<height-i-1;j++){
+                System.out.print(" ");
+            }
+            for (int j=0;j<=i;j++){
+                System.out.print((char)('a' + j));
+            }
+            for (int j=i-1;j>=0;j--){
+                System.out.print((char)('a'+j));
+            }
+            System.out.println();
+        }
+    }
+}
