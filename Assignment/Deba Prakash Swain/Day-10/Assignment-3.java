69a4f3a67cea16460241da2a628ceb006ef5db04
Deb Prakash Swain <150893454+PRAKASH-14A@users.noreply.github.com>
2024-06-20 13:53:45 +0530

    Create Assignment-3.java

---
+++ Assignment/Deba Prakash Swain/Day-10/Assignment-3.java
@@ -0,0 +1,13 @@
+public class Day10_Series {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        arr[0] = 2;
+        for (int i=1;i< arr.length;i++){
+           arr[i]=arr[i-1]+(i+2);
+        }
+        System.out.print("Series is: ");
+        for (int i=0;i< arr.length;i++){
+            System.out.print(arr[i]+",");
+        }
+    }
+}
