7c04f286e567080dfab6032d933e15e6c07681d7
Chinmaya5751 <chinmayachinuyoyo@gmail.com>
2024-06-20 10:46:29 +0530

    Add files via upload

---
+++ Assignment/Chinmaya Mohanta/day-9/pyramid.java
@@ -0,0 +1,25 @@
+import java.io.PrintStream;
+
+public class pyramid {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        PrintStream x = System.out;
+        x.println("This line is printed with x reference");
+
+        int spaces = 30;
+        int stars = 1;
+        for(int i=0; i<10; i=i+1) {
+            for(int j=0; j<spaces; j++) System.out.print(" ");
+            for(int j=0; j<stars; j++)  System.out.print("*");
+            System.out.println();
+            spaces--; stars += 2;
+        }
+    }
+}
+
+/*
+                              *
+                             ***
+                            *****
+
+*/
