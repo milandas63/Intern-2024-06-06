032d16d6cc63ef63a82995946e0f8afc5eafae82
Babul parida <babulparida625@gmail.com>
2024-06-21 12:04:27 +0530

    Add files via upload

---
+++ Assignment/Babul Parida/Day-10/Arithmetic.java
@@ -0,0 +1,10 @@
+public class ArithmeticSeries {
+    public static void main(String[] args) {
+        int start = 5;
+        int difference = 2;
+        int terms = 10;        
+        for (int i = 0; i < terms; i++) {
+            System.out.print(start +(i * difference) + " ");
+        }
+    }
+}
