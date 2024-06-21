249339d831f5b07d08ca3eb954c5eea9d718b995
mirabusalam <mirabusalam72@gmail.com>
2024-06-21 13:05:02 +0530

    Create Completeseries.java

---
+++ Assignment/Mir Abusalam/Day 10/Completeseries.java
@@ -0,0 +1,21 @@
+
+public class CompleteSeries {
+    public static void main(String[] args) 
+    {
+        int[] series = {2, 5, 9, 14, 20};
+        int n = 10;
+        int[] completeSeries = new int[n];
+        for(int i = 0; i < series.length; i++)
+         {
+
+            completeSeries[i] = series[i];
+        }
+        for(int i = series.length; i < n; i++) {
+            int nextDifference = (completeSeries[i - 1] - completeSeries[i - 2]) + 1;
+            completeSeries[i] = completeSeries[i - 1] + nextDifference;
+        }
+        for(int num : completeSeries) {
+            System.out.print(num + " ");
+        }
+    }
+}
