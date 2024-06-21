37cfabb58e577af4f9adcf613af688442d811b78
mirabusalam <mirabusalam72@gmail.com>
2024-06-21 13:06:24 +0530

    Create NumberSeries.java

---
+++ Assignment/Mir Abusalam/Day 10/NumberSeries.java
@@ -0,0 +1,12 @@
+
+public class NumberSeries {
+    public static void main(String[] args) {
+        int[] series = new int[10];
+        for (int i = 0; i < 10; i++) {
+            series[i] = i + 1;
+        }
+        for (int num : series) {
+            System.out.print(num + " ");
+        }
+    }
+}
