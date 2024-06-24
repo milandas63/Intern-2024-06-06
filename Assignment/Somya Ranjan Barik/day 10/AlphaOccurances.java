864dd8587f469da4c2d765104b142bdbd151df43
somya ranjan barik <171918964+SomyaBarik@users.noreply.github.com>
2024-06-24 10:37:19 +0530

    Add files via upload

---
+++ Assignment/Somya Ranjan Barik/day 10/AlphaOccurances.java
@@ -0,0 +1,26 @@
+public class AlphaOccurances {
+
+    public static void main(String[] args) {
+        String text = "This text will be taken from user";
+
+        //int count[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+        int[] count = new int[26];
+        for(int i=0; i<count.length; i++) count[i] = 0;
+        
+        char each;
+        for(int i=0; i<text.length(); i++) {
+            each = text.charAt(i);
+            if(each>=65 && each<=90) {
+                count[each-65]++;
+            } else if(each>=97 && each<=122) {
+                count[each-97]++;
+            }
+        }
+        for(int i=0; i<count.length; i++) {
+            if(count[i]>0) {
+                System.out.println((char)(i+65) + " = " + count[i]);
+            }
+        }
+    }
+
+}
