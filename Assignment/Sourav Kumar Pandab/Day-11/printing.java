a74d38b7637e5d881114b4351a4aa7229fcd43df
SOURAV KUMAR PANDAB <166332590+souravkumarpandab@users.noreply.github.com>
2024-06-25 14:14:36 +0530

    Add files via upload

---
+++ Assignment/Sourav Kumar Pandab/Day-11/printing.java
@@ -0,0 +1,20 @@
+public class printing {
+    public static void main(String[] args) {
+        String[] words = {"Hello", "World", "in", "a", "frame"};
+        printInFrame(words);
+    }
+    public static void printInFrame(String[] words) {
+        int maxLength = 0;
+        for (String word : words) {
+            if (word.length() > maxLength) {
+                maxLength = word.length();
+            }
+        }
+        String border = "*".repeat(maxLength + 4);
+        System.out.println(border);
+        for (String word : words) {
+            System.out.printf("* %-"+maxLength+"s *\n", word);
+        }
+        System.out.println(border);
+    }
+}
