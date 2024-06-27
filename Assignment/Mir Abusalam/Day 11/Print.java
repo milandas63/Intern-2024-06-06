e6ab877744b3d710e9da6ffc148efca6a90a1eb1
mirabusalam <mirabusalam72@gmail.com>
2024-06-27 11:00:07 +0530

    Create Print.java

---
+++ Assignment/Mir Abusalam/Day 11/Print.java
@@ -0,0 +1,21 @@
+
+public class print {
+    public static void main(String[] args) {
+        String[] words = {"Hello", "World", "in", "a", "frame"};
+        printInFrame(words);
+    }
+    public static void printInFrame(String[] words) {
+        int maxLength = 0;
+        for (String word : words) {
+            if (word.length() > maxLength) {
+                maxLength = word.length();
+            }
+        }
+        String border = "*".repeat(maxLength + 4);
+        System.out.println(border);
+        for (String word : words) {
+            System.out.printf("* %-"+maxLength+"s *\n", word);
+        }
+        System.out.println(border);
+    }
+}
