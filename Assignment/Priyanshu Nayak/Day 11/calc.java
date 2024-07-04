eaf4f192d8ed69688119865536d3f82571b152f0
priyanshu125125 <pn9261556@gmail.com>
2024-07-04 14:43:50 +0530

    Create calc.java

---
+++ Assignment/Priyanshu Nayak/Day 11/calc.java
@@ -0,0 +1,24 @@
+
+public class calcu {
+    public static void main(String[] args) {
+        System.out.println(unisum(728394));
+        System.out.println(unisum(9778911223L)); 
+        System.out.println(unisum(7978168568L)); 
+    }
+
+    public static int unisum(long number) {
+        while (number >= 10) {
+            number = sumOfDigits(number);
+        }
+        return (int) number;
+    }
+
+    private static long sumOfDigits(long number) {
+        long sum = 0;
+        while (number != 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+        return sum;
+    }
+}
