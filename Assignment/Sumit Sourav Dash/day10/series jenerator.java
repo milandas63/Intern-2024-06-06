6ed7fa658f9e3e748bafea81b1ac29aea3fe6c9d
Sumit sourav dash <ds6972943@gmail.com>
2024-07-05 11:25:06 +0530

    Update series jenerator.java

--- Assignment/Sumit Sourav Dash/day10/series jenerator.java
+++ Assignment/Sumit Sourav Dash/day10/series jenerator.java
@@ -1,1 +1,17 @@
-
+public class SeriesGenerator {
+        public static void main(String[] args) {
+            int[] series = new int[10];
+            series[0] = 1;
+            for (int i = 1; i < series.length; i++) {
+                if (i % 2 == 1) {
+                    series[i] = series[i - 1] * 2 + 1;
+                } else {
+                    series[i] = series[i - 1] * 2 - 1;
+                }
+            }
+            for (int number : series) {
+                System.out.print(number + " ");
+            }
+        }
+    }
+    
