3db559745554fb93105664cd62b5f825b1ce7652
soumyajitsamal001 <somyajitsamal3@gmail.com>
2024-07-12 12:41:17 +0530

    Add files via upload

---
+++ Assignment/Soumyajit Samal/Day-09/NumberPartternPrinting.java
@@ -0,0 +1,15 @@
+public class NumberPartternPrinting {
+    public static void main(String[] args) {
+        int n = 10;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
+
