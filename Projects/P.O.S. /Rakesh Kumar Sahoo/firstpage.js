708f6f55754ae4fc8a2ac410efe6130b0ec4a283
Rakesh kumar sahoo <rksahoo2023@gift.edu.in>
2024-08-01 15:21:59 +0530

    Add files via upload

---
+++ Projects/P.O.S. /Rakesh Kumar Sahoo/firstpage.js
@@ -0,0 +1,7 @@
+function login() {
+    alert("If you are Register then u Eligible for Login");
+    window.location.href = 'login.html';
+}
+function registration() {
+    window.location.href = 'register.html';
+}
