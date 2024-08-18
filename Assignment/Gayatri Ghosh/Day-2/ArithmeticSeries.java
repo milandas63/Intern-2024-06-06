705110d42ef7ce03d9614572b7174d27170b87fe
gayatri-ghosh27 <gayatrighosh008@gmail.com>
2024-08-18 18:11:13 +0530

    Add files via upload

---
+++ Assignment/Gayatri Ghosh/Day-2/ArithmeticSeries.java
@@ -0,0 +1,10 @@
+package Day_2;
+public class ArithmeticSeries {
+    public static void main(String[] args) {
+        int series = 5;
+        for(int i=0;i<10;i++) {
+        	System.out.print(series+" ");
+        	series+=2;
+        }
+    }
+}
---
+++ Assignment/Gayatri Ghosh/Day-2/NumSeries2.java
@@ -0,0 +1,8 @@
+package Day_2;
+public class NumSeries2 {
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i++) {
+        	System.out.print(i+" ");
+        }
+    }
+}
